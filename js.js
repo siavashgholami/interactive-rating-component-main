f78775f047f6797ffd0ba2299e89319f2d7dfa59
siavashgholami <13s65gh@gmail.com>
2023-11-10 08:25:40 -0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+const spans = document.querySelectorAll('.selectable-span');
+const result = document.querySelector('.result-span');
+const rating = document.querySelector('.rating');
+const thank = document.querySelector('.thank');
+
+spans.forEach(function(span) {
+    span.addEventListener('click', function() {
+        spans.forEach(function(s) {
+            s.classList.remove('selected');
+        })
+        this.classList.add('selected');
+
+        result.innerHTML = this.innerHTML;
+    });
+});
+
+document.getElementById('submit').addEventListener('click', function() {
+    rating.classList.add('visible');
+    thank.classList.remove('visible');
+});
